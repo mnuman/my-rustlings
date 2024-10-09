f2ac69185c480c56091866864e7d4dcd08575da8
Milco Numan <milco.numan@gmail.com>
2024-10-09 05:59:27 +0000

    Primitive types and conditionals

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -3,6 +3,6 @@ fn main() {
 
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
     // let /* your pattern here */ = cat;
-
+    let (name, age) = cat;      // Destructuring the tuple   
     println!("{name} is {age} years old");
 }
